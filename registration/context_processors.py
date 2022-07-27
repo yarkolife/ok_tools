fa1e96f3b4305f753b7fa856df518e79aa14da6d
schulztr <C.Schulz-Trieglaff@gmx.de>
2022-07-27 09:45:39 +0200

    use only one function to process the context in templates

--- registration/context_processors.py
+++ registration/context_processors.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 
 
-def ok_name(request):
-    """Make OK_NAME available in templates."""
-    return {'OK_NAME': settings.OK_NAME}
-
-
-def ok_name_short(request):
-    """Make OK_NAME_SHORT available in templates."""
-    return {'OK_NAME_SHORT': settings.OK_NAME_SHORT}
+def context(request):
+    """Make OK_NAME and OK_NAME_SHORT available in templates."""
+    return {
+        'OK_NAME': settings.OK_NAME,
+        'OK_NAME_SHORT': settings.OK_NAME_SHORT
+    }
