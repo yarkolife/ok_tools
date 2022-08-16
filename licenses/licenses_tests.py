1fb48116d0b07dfca21a7ee8175a5dbaf8838b48
schulztr <C.Schulz-Trieglaff@gmx.de>
2022-08-16 17:01:52 +0200

    add tests for license list

--- licenses/licenses_tests.py
+++ licenses/licenses_tests.py
@@ -11,6 +11,9 @@ HOME_URL = f'{DOMAIN}{reverse_lazy("home")}'
 LIST_URL = f'{DOMAIN}{reverse_lazy("licenses:licenses")}'
 CREATE_URL = f'{DOMAIN}{reverse_lazy("licenses:create")}'
 LOGIN_URL = f'{DOMAIN}{reverse_lazy("login")}'
+# the LR created first has the id 1
+DETAILS_URL = f'{DOMAIN}{reverse_lazy("licenses:details", args=[1])}'
+EDIT_URL = f'{DOMAIN}{reverse_lazy("licenses:update", args=[1])}'
 
 
 def test__licenses__views__ListLicensesView__1(browser, user):
@@ -28,6 +31,63 @@ def test__licenses__views__ListLicensesView__2(browser):
     """If no user is logged in the license overview returns a 404."""
     with pytest.raises(HTTPError, match=r'.*404.*'):
         browser.open(LIST_URL)
+
+
+def test__licenses__views__ListLicensesView__3(db, browser, license_request):
+    """All licenses of the user are listed."""
+    browser.login()
+    browser.open(LIST_URL)
+
+    assert str(license_request) in browser.contents
+    assert 'No' in browser.contents  # the License Request is not confirmed
+
+
+def test__licenses__views__ListLicensesView__4(browser, license_request):
+    """If a LR is confirmed, it is shown in the overview."""
+    license_request.confirmed = True
+    license_request.save()
+    browser.login()
+    browser.open(LIST_URL)
+
+    assert str(license_request) in browser.contents
+    assert 'Yes' in browser.contents  # the License Request is confirmed
+
+
+def test__licenses__views__DetailsLicensesView__1(browser, license_request):
+    """If no user is logged in the details view returns a 404."""
+    with pytest.raises(HTTPError, match=r'.*404.*'):
+        browser.open(DETAILS_URL)
+
+
+def test__licenses__views__DetailsLicensesView__2(browser, license_request):
+    """The details view can be reached using the Overview."""
+    browser.login()
+    browser.open(LIST_URL)
+    browser.follow(str(license_request))
+
+    assert f'Details for {str(license_request)}' in browser.contents
+    assert license_request.description in browser.contents
+
+
+def test__licenses__views__DetailsLicensesView__3(browser, license_request):
+    """It is possible to edit a LR over the details view."""
+    browser.login()
+    browser.open(DETAILS_URL)
+    browser.follow(id='id_edit_LR')
+
+    assert f'Edit {license_request}:' in browser.contents
+
+
+def test__licenses__views__UpdateLicensesView__1(browser, license_request):
+    """The Description can be changed using the edit site."""
+    browser.login()
+    browser.open(EDIT_URL)
+
+    new_description = "This is the new description."
+    browser.getControl('Description').value = new_description
+    browser.getControl('Save').click()
+
+    assert LicenseRequest.objects.get(description=new_description)
 
 
 def test__licenses__views__CreateLicenseView__1(browser, user):
@@ -58,7 +118,7 @@ def test__licenses__views__CreateLicenseView__3(browser, user):
     browser.getControl(name='duration_0').value = 0
     browser.getControl(name='duration_1').value = 0
     browser.getControl(name='duration_2').value = 10
-    browser.getControl('Create').click()
+    browser.getControl('Save').click()
 
     assert LIST_URL == browser.url
     assert 'Your licenses' in browser.contents
@@ -73,7 +133,7 @@ def test__licenses__views__CreateLicenseView__4(browser, user):
     browser.getControl(name='duration_0').value = 0
     browser.getControl(name='duration_1').value = 0
     browser.getControl(name='duration_2').value = 10
-    browser.getControl('Create').click()
+    browser.getControl('Save').click()
 
     assert CREATE_URL == browser.url
     assert 'This field is required' in browser.contents
