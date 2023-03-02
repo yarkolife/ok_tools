e7a549f9dfdf1692bd31332a60e7c6f41bfa9d50
Daniel Havlik <dh@gocept.com>
2023-03-02 13:02:28 +0100

    case correction

--- scripts/find_replace.py
+++ scripts/find_replace.py
@@ -12,27 +12,27 @@ def run():
 
     queryset = License.objects.all()
     for lc in queryset:
-        DESCRIPTION = lc.description and FIND in lc.description
-        TITLE = lc.title and FIND in lc.title
-        SUBTITLE = lc.subtitle and FIND in lc.subtitle
+        description = lc.description and FIND in lc.description
+        title = lc.title and FIND in lc.title
+        subtitle = lc.subtitle and FIND in lc.subtitle
 
-        if not (DESCRIPTION or TITLE or SUBTITLE):
+        if not (description or title or subtitle):
             continue
 
-        CONFIRMED = lc.confirmed
+        confirmed = lc.confirmed
 
-        if CONFIRMED:
+        if confirmed:
             # If the license is already confirmed we need to unconfirm them
             # before we can make our changes.
             lc.confirmed = False
             lc.save(update_fields=['confirmed'])
 
-        if DESCRIPTION:
+        if description:
             lc.description = lc.description.replace(FIND, REPLACE)
-        if TITLE:
+        if title:
             lc.title = lc.title.replace(FIND, REPLACE)
-        if SUBTITLE:
+        if subtitle:
             lc.subtitle = lc.subtitle.replace(FIND, REPLACE)
 
-        lc.confirmed = CONFIRMED
+        lc.confirmed = confirmed
         lc.save()
