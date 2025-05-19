fd2a61deae8039099f99755b2cd5eed2754d803b
OffenerKanalMQ <128150086+OffenerKanalMQ@users.noreply.github.com>
2025-05-19 17:41:06 +0200

    Update models.py (#165)
    * Update models.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- inventory/models.py
+++ inventory/models.py
@@ -2,6 +2,7 @@ from .middleware import get_current_user
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.core.files.storage import FileSystemStorage
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.validators import FileExtensionValidator
 from django.db import models
@@ -10,10 +11,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from pathlib import Path
+from tempfile import gettempdir
 import logging
+import os
 
 
 logger = logging.getLogger('django')
+
+tmp_import_storage = FileSystemStorage(
+    location=os.path.join(gettempdir(), "inventory_import")
+)
 
 
 class Manufacturer(models.Model):
@@ -202,16 +209,15 @@ class InventoryImport(models.Model):
     def timestamp_path(instance, filename):
         """Create a path based on the current timestamp."""
         now = datetime.now()
-        path = Path(f"{now.year}/{now.month}/{now.day}")
         ext = Path(filename).suffix
-        return path / f"{now.hour}-{now.minute}-{now.second}-{now.microsecond}{ext}"
+        return f"{now.year}/{now.month}/{now.day}/" \
+            f"{now.hour}-{now.minute}-{now.second}-{now.microsecond}{ext}"
 
     file = models.FileField(
         verbose_name=_('Inventory file'),
         upload_to=timestamp_path,
-        validators=[
-            FileExtensionValidator(allowed_extensions=['xlsx', 'csv']),
-        ],
+        storage=tmp_import_storage,
+        validators=[FileExtensionValidator(["xlsx", "csv"])],
         blank=False,
         null=False,
     )
