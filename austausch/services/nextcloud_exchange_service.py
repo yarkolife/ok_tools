3b6d88585e6bc600a02ff2d1f597f18578051820
Pavlo <kozakov@okmq.de>
2026-03-14 02:03:01 +0100

    Try chunk assembly MOVE from upload directory instead of .file

--- austausch/services/nextcloud_exchange_service.py
+++ austausch/services/nextcloud_exchange_service.py
@@ -808,8 +808,8 @@ class NextcloudExchangeService:
 
                     logger.debug("Uploaded chunk %s (%s bytes)", chunk_name, len(chunk))
 
-            # 3. MOVE .file to assemble
-            assemble_url = f"{upload_dir_url}/.file"
+            # 3. MOVE upload directory to assemble
+            assemble_url = upload_dir_url
             move_headers = {
                 'Destination': destination_url,
                 'OC-Total-Length': str(file_size),
@@ -1174,7 +1174,7 @@ class NextcloudExchangeService:
                     if progress_callback:
                         progress_callback(chunk_num, total_chunks, progress, speed_mbps)
 
-            assemble_url = f"{upload_dir_url}/.file"
+            assemble_url = upload_dir_url
             move_headers = {
                 'Destination': destination_url,
                 'OC-Total-Length': str(file_size),
